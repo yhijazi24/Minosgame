1d359088fea1d3bbbe9ef6959602c5398e7778e9
elscelina <126341943+elscelina@users.noreply.github.com>
2025-01-31 01:39:27 +0100

    Added cmake for linux

---
+++ CMakeFiles/Minosgames.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Minosgames.dir/dominos/dominos_events.c.o"
+  "CMakeFiles/Minosgames.dir/dominos/dominos_game.c.o"
+  "CMakeFiles/Minosgames.dir/dominos/dominos_renderer.c.o"
+  "CMakeFiles/Minosgames.dir/dominos/dominos_types.c.o"
+  "CMakeFiles/Minosgames.dir/game.c.o"
+  "CMakeFiles/Minosgames.dir/game_score.c.o"
+  "CMakeFiles/Minosgames.dir/main.c.o"
+  "CMakeFiles/Minosgames.dir/menu.c.o"
+  "CMakeFiles/Minosgames.dir/player_setup.c.o"
+  "CMakeFiles/Minosgames.dir/triominos/triomino_events.c.o"
+  "CMakeFiles/Minosgames.dir/triominos/triomino_game.c.o"
+  "CMakeFiles/Minosgames.dir/triominos/triomino_renderer.c.o"
+  "CMakeFiles/Minosgames.dir/triominos/triomino_types.c.o"
+  "Minosgames"
+  "Minosgames.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Minosgames.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
